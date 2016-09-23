2c21d5f1bd1ccce13231d1867336a2cbc80f7a06
Nate Barbettini <nathanaelb@gmail.com>
2016-09-23 13:32:05 -0700

    Add some niceties

--- src/StormpathCustomSamlDemo/Controllers/LoginServicesController.cs
+++ src/StormpathCustomSamlDemo/Controllers/LoginServicesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Stormpath.SDK;
@@ -18,21 +19,62 @@ namespace StormpathCustomSamlDemo.Controllers
         }
 
         [Route("[controller]/getAccountType")]
-        public async Task<IActionResult> GetAccountType(string email)
+        public async Task<IActionResult> GetAccountType(string email, CancellationToken ct)
         {
             if (string.IsNullOrEmpty(email))
             {
                 return Json(null);
             }
 
-            var account = await _stormpathApplication.GetAccounts().Where(a => a.Email == email).SingleOrDefaultAsync();
+            var account = await _stormpathApplication.GetAccounts()
+                .Where(a => a.Email == email)
+                .SingleOrDefaultAsync(ct);
 
             if (account == null)
             {
                 return Json(null);
             }
 
-            return Json(new {href = account.Href});
+            var directory = await account.GetDirectoryAsync(ct);
+            var provider = await directory.GetProviderAsync(ct);
+
+            return Json(new {type = provider.ProviderId.ToLower()});
+        }
+
+        [Route("[controller]/samlRedirect")]
+        public async Task<IActionResult> GetSamlRedirect(string email, string next, CancellationToken ct)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest();
+            }
+
+            var account = await _stormpathApplication.GetAccounts()
+                .Where(a => a.Email == email)
+                .SingleOrDefaultAsync(ct);
+
+            if (account == null)
+            {
+                return BadRequest();
+            }
+
+            var directory = await account.GetDirectoryAsync(ct);
+
+            var samlUrlBuilder = await _stormpathApplication.NewSamlIdpUrlBuilderAsync(ct);
+            var redirectUrl = samlUrlBuilder
+                .SetCallbackUri("http://localhost:5000/stormpathCallback")
+                .SetAccountStore(directory.Href);
+
+            if (!string.IsNullOrEmpty(next))
+            {
+                redirectUrl.SetState(next); // Can save a relative path in the state, then redirect after the callback
+            }
+
+            HttpContext.Response.Headers.Add("Cache-control", "no-cache, no-store");
+            HttpContext.Response.Headers.Add("Pragma", "no-cache");
+            HttpContext.Response.Headers.Add("Expires", "-1");
+
+            return Redirect(redirectUrl.Build());
         }
     }
 }
---
+++ src/StormpathCustomSamlDemo/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stormpath.SDK.Account;
+using Stormpath.SDK.Client;
+
+namespace StormpathCustomSamlDemo.Controllers
+{
+    namespace StormpathExample.Controllers
+    {
+        // This API controller is protected and requires the user to be logged in.
+        // For authorization policies, see the comments in Startup.
+        [Authorize]
+        public class ProfileController : Controller
+        {
+            private readonly IClient _stormpathClient;
+            private readonly IAccount _stormpathAccount;
+
+            public ProfileController(IClient stormpathClient, Lazy<IAccount> stormpathAccount)
+            {
+                // Stormpath request objects injected via DI
+                _stormpathClient = stormpathClient;
+                _stormpathAccount = stormpathAccount.Value;
+            }
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
+}
